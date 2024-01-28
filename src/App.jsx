1955b3a6249e9f7908b875f8232b045f2af7f24e
Sarah Leder <Trinity16201@gmail.com>
2024-01-28 15:34:34 -0500

    Updated files to create functionality and began editing About Me page for inital setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,17 @@
-import React, { useState } from "react"
-import "./index.css";
-import Header from "../components/Header";
-import AboutMe from "../components/AboutMe";
-import Portfolio from "../components/Portfolio";
-import Resume from "../components/Resume";
-import Contact from "../components/Contact";
-import Footer from "../components/Footer";
-import { Helmet } from "react-helmet-async";
-
+import { Outlet } from 'react-router-dom';
+import Nav from '/components/Nav';
+import './index.css';
+import Footer from './pages/Footer'
 
 function App() {
-  const [currentTab, handleTabChange] = useState("About");
-  const renderTab = () => {
-    if (currentTab === "About") {
-      return <AboutMe />;
-    }
-    if (currentTab === "Portfolio") {
-      return <Portfolio />;
-    }
-    if (currentTab === "Resume") {
-      return <Resume />;
-    }
-    if (currentTab === "Contact") {
-      return <Contact />;
-    }
-    return <AboutMe />;
-  };
   return (
     <>
-      <Helmet>
-        <title>Sarah Leder's Portfolio | {currentTab} </title>
-      </Helmet>
-      ;
-      <Header
-        currentTab={currentTab}
-
-        handleTabChange={handleTabChange}
-      ></Header>
-      <main>{renderTab()}</main>
-      <Footer></Footer>
+      <Nav />
+      <main className="mx-3">
+        <Outlet />
+      </main>
+      <Footer/>
     </>
-    
   );
 }
 
---
+++ src/pages/AboutMe.jsx
@@ -0,0 +1,13 @@
+export default function AboutMe() {
+    return (
+      <div className="about">
+        <h1>Sarah Leder</h1>
+        <img className= "my-pic" src="/Profile-pic.jpg" alt="Image of Sarah Leder" />
+        <h2 className="details">
+          I've been in the veterinary industry for 17 years working in both
+          practice and reference laboratories. In August 2023, I began a new
+          journey into full stack web development.
+        </h2>
+      </div>
+    );
+  }
