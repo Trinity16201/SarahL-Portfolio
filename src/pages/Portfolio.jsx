46da5448d0d584a2356e43726ad249d1a2c26ca1
Sarah Leder <Trinity16201@gmail.com>
2024-01-28 19:06:05 -0500

    Added all 6 portfolio items- worked on styling

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -2,13 +2,10 @@ export default function Portfolio() {
   return (
     <div class="row">
       <h1>Portfolio</h1>
-      <div class="column">
-        <div class="card">
+      <div className="column">
+        <div className="card">
           <h2>Marvel Quiz </h2>
-          <img
-            className="portfolio-pic"
-            src="/Marvel.png"
-          />
+          <img className="portfolio-pic" src="/Marvel.png" />
           <p>
             {" "}
             The Marvel Personality Quiz is a fun and interactive web application
@@ -25,16 +22,14 @@ export default function Portfolio() {
         </div>
       </div>
 
-      <div class="column">
-        <div class="card">
+      <div className="column">
+        <div className="card">
           <h2>Nerdmazon </h2>
-          <img
-            className="portfolio-pic"
-            src="/Doctor-who-hat.jpg"
-          />
+          <img className="portfolio-pic" src="/Doctor-who-hat.jpg" />
           <p>
             {" "}
-            Experience shopping with only products to meet your Nerdy needs. Browse your favorite shows, movies, games, and coding gear.
+            Experience shopping with only products to meet your Nerdy needs.
+            Browse your favorite shows, movies, games, and coding gear.
           </p>
           {/* <a href="https://cryptic-castle-11006-b2c2b99712fd.herokuapp.com"</a> */}
           <button>Website</button>
@@ -42,21 +37,64 @@ export default function Portfolio() {
           <button>Github</button>
         </div>
       </div>
-      <div class="column">
-        <div class="card">..</div>
+      <div className="column">
+        <div className="card">
+          <h2>Work Day Scheduler </h2>
+          <img className="portfolio-pic" src="/WorkDay.PNG" />
+          <p>
+            {" "}
+            This application provides an easy to use daily calendar. You can add
+            events to each hour to schedule your work day. Color coding is used
+            to show if the current time is before, during, or after the hour.
+            You can aslo delete events as needed.
+          </p>
+          {/* <a href="https://trinity16201.github.io/Create-Calendar/"</a> */}
+          <button>Website</button>
+          {/* <a href="https://github.com/Trinity16201/Create-Calendar.git"</a> */}
+          <button>Github</button>
+        </div>
+        <div className="card">
+          <h2>Password Generator </h2>
+          <img className="portfolio-pic" src="/Password.jpg" />
+          <p>
+            {" "}
+            Create your own unique password by using this password generator
+            application.
+          </p>
+          {/* <a href="https://trinity16201.github.io/Password-Generator/"</a> */}
+          <button>Website</button>
+          {/* <a href="https://github.com/Trinity16201/Password-Generator.git"</a> */}
+          <button>Github</button>
+        </div>
       </div>
-
-      <div class="row">
-        <div class="card">..</div>
+      <div className="column">
+        <div className="card">
+          <h2>Weather Dashboard </h2>
+          <img className="portfolio-pic" src="/Weather.JPG" />
+          <p>
+            {" "}
+            Utilizing an API, this application looks up current weather
+            information and a 5 day forcast for your area.
+          </p>
+          {/* <a href="https://trinity16201.github.io/Weather-Dash/"</a> */}
+          <button>Website</button>
+          {/* <a href="https://github.com/Trinity16201/Weather-Dash.git"</a> */}
+          <button>Github</button>
+        </div>
       </div>
-      <div class="column">
-        <div class="card">..</div>
-      </div>
-      <div class="column">
-        <div class="card">..</div>
-      </div>
-      <div class="column">
-        <div class="card">..</div>
+      <div className="column">
+        <div className="card">
+        <h2>My first portfolio</h2>
+          <img className="portfolio-pic" src="/First-Portfolio.JPG" />
+          <p>
+            {" "}
+            This page was the first portfolio I created early on in my development process. 
+          </p>
+          {/* <a href="https://trinity16201.github.io/Personal-Portfolio/"</a> */}
+          <button>Website</button>
+          {/* <a href="https://github.com/Trinity16201/Personal-Portfolio.git"</a> */}
+          <button>Github</button>
+        </div>
       </div>
     </div>
   );
