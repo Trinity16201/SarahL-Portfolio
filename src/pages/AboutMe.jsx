65fe4caa3931e996fb672534323e6e6f5d95d097
Sarah Leder <Trinity16201@gmail.com>
2024-01-28 19:55:46 -0500

    worked on images of resume and added link for download

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,13 +1,19 @@
 export default function AboutMe() {
-    return (
-      <div className="about">
-        <h1>Sarah Leder</h1>
-        <img className= "my-pic" src="/Profile-pic.jpg" alt="Image of Sarah Leder" />
-        <h2 className="details">
-          I've been in the veterinary industry for 17 years working in both
-          practice and reference laboratories. In August 2023, I began a new
-          journey into full stack web development.
-        </h2>
-      </div>
-    );
-  }
+  return (
+    <div className="about">
+      <h1>Sarah Leder</h1>
+      <img
+        className="my-pic"
+        src="/Profile-pic.jpg"
+        alt="Image of Sarah Leder"
+      />
+      <h2 className="details">
+        I've been in the veterinary industry for 17 years working in both
+        practice and reference laboratories. In August 2023, I began a new
+        journey into full stack web development. I was driven by the need to
+        understand websites out of curiosity and looking for a challenging
+        enviroment.
+      </h2>
+    </div>
+  );
+}
