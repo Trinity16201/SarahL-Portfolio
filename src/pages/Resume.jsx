5b457d72f2a771945de376d42c45a720755305bf
Sarah Leder <Trinity16201@gmail.com>
2024-01-30 20:05:18 -0500

    fixed Resume button bug

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,11 +1,12 @@
+import Resume from '../../assets/ResumePDF.pdf'
 export default function ResumePage() {
   return (
     <>
       <h1>Resume</h1>
       <img className="resume-pic" src="/Resume1.PNG" />
       <img className="resume-pic" src="/Resume2.PNG" />
-      <a href="/assets/ResumePDF.pdf">
-        <h2>Download Resume</h2>
+      <a href= {Resume} download="Resume" target="_blank">
+        <button className="resume m-5">Download Resume here</button>
       </a>
     </>
   );
